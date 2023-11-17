7390ded9f1834b9e7c907be6aeee285b52cc273c
Nullifidian <rick.romig@gmail.com>
2023-11-17 15:50:46 -0500

    Added feh commands to randomly select wallpaper at predetermined intervals.

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -1,16 +1,19 @@
-
+#!/usr/bin/env bash
 # Autostart applications
 # /usr/lib/x86_64-linux-gnu/polkit-mate/polkit-mate-authentication-agent-1 &
 /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 ~/.local/bin/polybar-i3 &
 # nitrogen --restore; sleep 1; 
 feh --bg-fill ~/.config/backgrounds/blackcat_1920x1080.png &
+# feh --bg-fill --randomize ~/Pictures/wallpaper/* &
+# while true; do feh --bg-fill --no-fehbg --randomize ~/Pictures/wallpaper/*; sleep 300; done &
 picom -b &
+udiskie &
 numlockx on &
 nm-applet &
 volumeicon &
 dunst &
-blueman-applet &
+lsusb | grep -iq blue && blueman-applet &
 
 # sxhkd
 sxhkd -c ~/.config/i3/sxhkd/sxhkdrc &
