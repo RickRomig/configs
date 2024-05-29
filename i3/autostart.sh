43d88f2335f34ff33dc6c1e70c3c27980fabbbf6
Nullifidian <rick.romig@gmail.com>
2024-05-29 19:09:08 -0400

    i3/autostart.sh - changed path to polybar-i3, changed feh option

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -2,10 +2,10 @@
 # Autostart applications
 # /usr/lib/x86_64-linux-gnu/polkit-mate/polkit-mate-authentication-agent-1 &
 /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
-~/.local/bin/polybar-i3 &
+~/.config/polybar/polybar-i3 &
 # nitrogen --restore; sleep 1;
-feh --bg-fill ~/.config/backgrounds/blackcat_1920x1080.png &
-# while true; do feh --bg-fill --no-fehbg --randomize ~/Pictures/wallpaper/*; sleep 600; done &
+# feh --bg-max ~/.config/backgrounds/blackcat_1920x1080.png &
+while true; do feh --bg-max --no-fehbg --randomize ~/Pictures/wallpaper/*; sleep 600; done &
 picom -b &
 udiskie &
 numlockx on &
@@ -13,6 +13,5 @@ nm-applet &
 volumeicon &
 dunst &
 lsusb | grep -iq blue && blueman-applet &
-
 # sxhkd
 sxhkd -c ~/.config/i3/sxhkd/sxhkdrc &
