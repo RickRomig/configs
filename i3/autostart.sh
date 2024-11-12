c91a1b375f5af0a77e3d7fec2c5655dda5ec0e93
Nullifidian <rick.romig@gmail.com>
2024-11-11 20:29:06 -0500

    i3/autostart.sh - cleaned up and added command from debian-i3

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -4,26 +4,33 @@ start() {
 }
 
 # Autostart applications
-# /usr/lib/x86_64-linux-gnu/polkit-mate/polkit-mate-authentication-agent-1 &
 /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
-~/.config/polybar/polybar-i3 &
-# nitrogen --restore; sleep 1;
+
+# bar start
+~/.config/i3/polybar-i3 &
+# ~/.config/polybar/polybar-i3 &
+
+# wallpaper
 # feh --bg-max ~/.config/backgrounds/blackcat_1920x1080.png &
 while true; do feh --bg-fill --no-fehbg --randomize ~/Pictures/wallpaper/*; sleep 600; done &
+
+# compositor and notifications
+picom --animations -b &
+# picom -b &
+numlockx off &
+dunst &
+lsusb | grep -iq blue && blueman-applet &
 
 # Systray apps
 nm-applet &
 start volumeicon &
-lsusb | grep -iq blue && blueman-applet &
 killall -q xfce4-power-manager
 xfce4-power-manager --daemon
 
 # Utilities in the background
-picom -b &
 udiskie &
-numlockx off &
-dunst &
 start redshift -c ~/.config/redshift.conf &
 
 # sxhkd
-sxhkd -c ~/.config/i3/sxhkd/sxhkdrc &
+# sxhkd -c ~/.config/i3/sxhkd/sxhkdrc &
+sxhkd -c ~/.config/i3/sxhkdrc &
