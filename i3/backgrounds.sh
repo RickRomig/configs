c8b73bf50e7f74b4738ace75316671d176f1077c
Nullifidian <rick.romig@gmail.com>
2024-12-08 08:46:35 -0500

    i3/backgrounds.sh - changed home_or_away() to check if wired connection is up to determine background folder. Added notification to show background source, case based on hostname to determine which function to use.

--- i3/backgrounds.sh
+++ i3/backgrounds.sh
@@ -7,7 +7,7 @@
 # Author       : Copyright © 2024 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 13 Nov 2024
-# Last updated : 13 Nov 2024, Version: 1.0.24318
+# Last updated : 08 Dec 2024, Version: 1.1.24343
 # Comments     :
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
@@ -18,24 +18,43 @@ set -eu
 ## Functions ##
 
 home_or_away() {
-	local wifi
-	wifi=$(/sbin/iw dev wlp2s0 link | awk '/SSID/ {print $2}')
-	if [[ "$wifi" == "mosfanet" ]]; then
+	# local wifi
+	# wifi=$(/sbin/iw dev wlp2s0 link | awk '/SSID/ {print $2}')
+	# if [[ "$wifi" == "mosfanet" ]]; then
+	if grep -w up /sys/class/net/enp0s31f6/operstate; then
+		notify-send -t 2500 "Baqckgrounds" "Pictures/wallpaper"
 	  while true
 	  	do feh --bg-fill --no-fehbg --randomize /home/rick/Pictures/wallpaper/*
 	  done &
 	else
+		notify-send -t 2500 "Baqckgrounds" ".config/backgrounds"
 	  while true
 	  	do feh --bg-fill --no-fehbg --randomize /home/rick/.config/backgrounds/*
 	  done &
 	fi
 }
 
-home_only() {
+home_nsfw() {
+	notify-send -t 2500 "Baqckgrounds" "Pictures/wallpaper"
 	while true
 		do feh --bg-fill --no-fehbg --randomize /home/rick/Pictures/wallpaper/*
 	done &
 }
 
-home_or_away
-# home_only
+home_sfw() {
+	notify-send -t 2500 "Baqckgrounds" ".config/backgrounds"
+	while true
+		do feh --bg-fill --no-fehbg --randomize /home/rick/.config/backgrounds/*
+	done &
+}
+
+main() {
+	local_host="${HOSTNAME:-$(hostname)}"
+	case "$local_host" in
+		hp-858-g3 ) home_or_away ;;
+		hp-8300 ) home_sfw ;;
+		* ) home_nsfw
+	esac
+}
+
+main "$@"
