7d9d4567a8a40c11c509051fd100cc3601287ed5
Richard Romig <rick.romig@gmail.com>
2025-04-29 15:45:14 -0400

    i3/autostart.sh: changed sxhkd to start in i3 directory (not i3/sxhkd)

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -25,8 +25,8 @@ numlockx off &
 dunst &
 
 # sxhkd
-sxhkd -c ~/.config/i3/sxhkd/sxhkdrc &
-# sxhkd -c ~/.config/i3/sxhkdrc &
+# sxhkd -c ~/.config/i3/sxhkd/sxhkdrc &
+sxhkd -c ~/.config/i3/sxhkdrc &
 
 # Utilities in the background
 udiskie &
