81f27de3e9dbc7f2ea34b1f43afbfba3085f72f2
Richard Romig <rick.romig@gmail.com>
2025-06-14 09:59:42 -0400

    autostart.sh: added header comments

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -1,35 +1,36 @@
 #!/usr/bin/env bash
+###############################################################################
+# Script Name  : autostart.sh
+# Description  : i3 autostart script
+# Dependencies : None
+# Arguments    : None
+# Author       : Copyright © 2025, Richard B. Romig, Mosfanet
+# Email        : rick.romig@gmail.com | rick.romig@mymetronet.com
+# Created      : 18 Nov 2021
+# Last updated : 14 Jun 2025
+# Version      : 2.0.25165
+# Comments     : New installs - sxhkdrc will be in i3 directory not sxhkd subdirectory
+# TODO (Rick)  :
+# License      : GNU General Public License, version 2.0
+###############################################################################
 
 start() {
   [[ -z "$(pgrep -f "$1")" ]] && ${2:-$1} &
 }
 
-# Autostart applications
 /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
-
-# status bar start
 ~/.config/polybar/polybar-i3 &
-
-# systray apps
 nm-applet &
 start volumeicon &
 killall -q xfce4-power-manager
 xfce4-power-manager --daemon
 lsusb | grep -iq blue && blueman-applet &
-
-# compositor, numlock, notifications
 picom -b &
 numlockx off &
 dunst &
-
-# sxhkd
 # sxhkd -c ~/.config/i3/sxhkd/sxhkdrc &
 sxhkd -c ~/.config/i3/sxhkdrc &
-
-# Utilities in the background
 pgrep -f udiskie &>/dev/null || udiskie &
 pgrep -f redshift | xargs -n1 kill -9
 redshift -c ~/.config/redshift.conf &
-
-# wallpapers
 pgrep -f backgrounds.sh &>/dev/null || ~/.config/i3/backgrounds.sh
