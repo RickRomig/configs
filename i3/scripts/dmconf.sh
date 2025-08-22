0519d1d19fa6c6c717a1d951e103cfd014a92890
Richard Romig <rick.romig@gmail.com>
2025-08-22 11:53:44 -0400

    scripts/dmconf.sh: added GPL license info

--- i3/scripts/dmconf.sh
+++ i3/scripts/dmconf.sh
@@ -7,13 +7,21 @@
 # Author       : Copyright © 2023, Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.com
 # Created      : 22 Nov 2023
-# Last updated : 15 Aug 2025
+# Last updated : 22 Aug 2025
 # Comments     :
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
 ###############################################################################
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 
-# Define text editor to use:
 DMENUEDITOR="mousepad"
 
 # array to choose from:
