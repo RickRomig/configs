174225dfd1c94f61ae1b2de1379cd2321a88b0f7
Richard Romig <rick.romig@gmail.com>
2025-08-22 11:48:34 -0400

    scripts/check_updates.sh: added header comments and GPL license info

--- i3/scripts/check_updates.sh
+++ i3/scripts/check_updates.sh
@@ -1,4 +1,27 @@
 #!/usr/bin/env sh
+###############################################################################
+# Script Name  : check_updates.sh
+# Description  : Checks for available updates
+# Dependencies : None
+# Arguments    : None
+# Author       : Copyright © 2025, Richard B. Romig, Mosfanet
+# Email        : rick.romig@gmail.com | rick.romig@mymetronet.com
+# Created      : 22 Aug 2025
+# Last updated : 22 Aug 2025
+# Comments     : Original script by Drew Grif @ https://github.com/drewgrif
+# TODO (Rick)  :
+# License      : GNU General Public License, version 2
+# License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
+###############################################################################
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 
 updates=$(apt list --upgradable 2>/dev/null | grep -c 'upgradable')
 
