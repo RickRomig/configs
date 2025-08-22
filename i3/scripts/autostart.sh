4d23c421379a99fda8e98cba11d480715fb3d095
Richard Romig <rick.romig@gmail.com>
2025-08-22 11:46:11 -0400

    scripts/autostart.sh: added GPL notice

--- i3/scripts/autostart.sh
+++ i3/scripts/autostart.sh
@@ -7,12 +7,21 @@
 # Author       : Copyright © 2025, Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.com
 # Created      : 18 Nov 2021
-# Last updated : 14 Aug 2025
-# Version      : 2.1.25226
+# Last updated : 22 Aug 2025
+# Version      : 2.1.25234
 # Comments     : New installs - sxhkdrc will be in i3 directory not sxhkd subdirectory
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
 ###############################################################################
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 
 start() {
   [[ -z "$(pgrep -f "$1")" ]] && ${2:-$1} &
