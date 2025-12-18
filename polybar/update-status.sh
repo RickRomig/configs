872b08beadb064a45884ca9c4e1b82464984be32
Richard Romig <rick.romig@gmail.com>
2025-12-17 21:36:48 -0500

    update-status.sh: removed UPD from echo (not needed)

--- polybar/update-status.sh
+++ polybar/update-status.sh
@@ -2,4 +2,4 @@
 # Polybar module to check for updates
 # sources: https://github.com/x70b1/polybar-scripts, https://github.com/raven2cz/polybar-config/blob/main/scripts/check-all-updates.sh
 updates=$(LANG=C apt-get upgrade -s | grep -P '^\d+ upgraded'| cut -d" " -f1)
-echo "UPD $updates"
+echo "$updates"
