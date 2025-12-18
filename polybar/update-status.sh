2fbb6af6515e971ff268393860a3b8773cd35492
Richard Romig <rick.romig@gmail.com>
2025-12-17 21:59:23 -0500

    update-status.sh: removed 'LANG=C' was preventing updates from being counted.

--- polybar/update-status.sh
+++ polybar/update-status.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Polybar module to check for updates
 # sources: https://github.com/x70b1/polybar-scripts, https://github.com/raven2cz/polybar-config/blob/main/scripts/check-all-updates.sh
-updates=$(LANG=C apt-get upgrade -s | grep -P '^\d+ upgraded'| cut -d" " -f1)
+updates=$(apt-get upgrade -s | grep -P '^\d+ upgraded'| cut -d" " -f1)
 echo "$updates"
