f474387ca3ee700bb1a9a330613eb50d596bf07e
Richard Romig <rick.romig@gmail.com>
2026-02-14 19:48:44 -0500

    dmconf.sh: refactored. Implemented associative array and main()

--- i3/scripts/dmconf.sh
+++ i3/scripts/dmconf.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 ###############################################################################
 # Script Name  : dmconf.sh
-# Description  : select from a list of config files to edit
+# Description  : Dmenu script to exit configuration files
 # Dependencies : dmenu
 # Arguments    : None
 # Author       : Copyright © 2023, Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.com
 # Created      : 22 Nov 2023
-# Last updated : 22 Aug 2025
+# Last updated : 14 Feb 2026
+# Version      : 2.0.26045
 # Comments     :
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2.0
@@ -22,38 +23,36 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 
-DMENUEDITOR="mousepad"
+DMENUEDITOR="/usr/bin/mousepad"
 
-# array to choose from:
-declare -a options=(
-"aliases - $HOME/.bash_aliases"
-"bashrc - $HOME/.bashrc"
-"profile - $HOME/.profile"
-"bat - $HOME/.config/bat/config"
-"codium -$HOME/.config/VSCodium/User/settings.json"
-"i3 autostart - $HOME/.config/i3/scripts/autostart.sh"
-"i3 config - $HOME/.config/i3/config"
-"i3 rules - $HOME/.config/i3/rules.conf"
-"i3 workspaces - $HOME/.config/i3/workspaces.conf"
-"kitty - $HOME/.config/kitty/kitty.conf"
-"logout - $HOME/.config/i3/scripts/logout"
-"micro - $HOME/.config/micro/settings.json"
-"fastfetch - $HOME/.config/fastfetch/config.jsonc"
-"polybar - $HOME/.config/polybar/config.ini"
-"sxhkdrc - $HOME/.config/i3/sxhkdrc"
-"quit"
+declare -rA config_files=(
+	[aliases]="$HOME/.bash_aliases"
+	[bashrc]="$HOME/.bashrc"
+	[profile]="$HOME/.profile"
+	[bat]="$HOME/.config/bat/config"
+	[codium]="$HOME/.config/VSCodium/User/settings.json"
+	[i3-autostart]="$HOME/.config/i3/scripts/autostart.sh"
+	[i3-config]="$HOME/.config/i3/config"
+	[i3-rules]="$HOME/.config/i3/rules.conf"
+	[i3-workspaces]="$HOME/.config/i3/workspaces.conf"
+	[sxhkdrc]=" $HOME/.config/i3/sxhkdrc"
+	[kitty]="$HOME/.config/kitty/kitty.conf"
+	[micro]="$HOME/.config/micro/settings.json"
+	[fastfetch]="$HOME/.config/fastfetch/config.jsonc"
+	[polybar]="$HOME/.config/polybar/config.ini"
+	[logout]="$HOME/.config/i3/scripts/power.sh"
+	[quit]="No configuration file selected."
 )
 
-# Pipe the array into dmenu
-choice=$(printf '%s\n' "${options[@]}" | dmenu -i -l 15 -p 'Edit config:')
-
-if [[ "$choice" == "quit" ]]; then
-	echo "Program terminated." && exit
-elif [[ "$choice" ]]; then
-	cfg_path=$(printf '%s\n' "${choice}" | awk '{print $NF}')
-	[[ -f "$cfg_path" ]] || { printf "%s configuration not found.\n" "${cfg_path##*/}" >&2; exit 2; }
+main() {
+	local choice cfg_path
+	# Pipe the array into dmenu
+	choice=$(printf '%s\n' "${!config_files[@]}" | dmenu -i -l 16 -p 'Edit config:')
+	[[ "$choice" == "quit" || -z "$choice" ]] && { printf "No configuration file selected.\n"; exit; }
+	cfg_path="${config_files[$choice]}"
+	[[ -f "$cfg_path" ]] || { printf "%s: %s not found.\n" "$choice" "${cfg_path##*/}" >&2; exit 2; }
 	"$DMENUEDITOR" "$cfg_path"
-else
-	echo "Program terminated."
-fi
-exit
+	exit
+}
+
+main "$@"
