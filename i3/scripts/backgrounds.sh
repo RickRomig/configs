2411271ce7b03c75515927018c5bc9bba226fabb
Richard Romig <rick.romig@gmail.com>
2026-09-13 13:44:16 -0400

    i3/scripts/backgrounds.sh: updated indentation

--- i3/scripts/backgrounds.sh
+++ i3/scripts/backgrounds.sh
@@ -1,26 +1,27 @@
 #!/usr/bin/env bash
-##########################################################################
+###############################################################################
 # Script Name  : backgrounds.sh
-# Description  : Set wallpaper directory
+# Description  : Set wallpaper directory in i3 X-session
 # Dependencies : feh
 # Arguments    : None
 # Author       : Copyright © 2024 Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.net
 # Created      : 13 Nov 2024
-# Last updated : 13 Jan 2026
-# Version      : 3.0.26013
+# Last updated : 13 Sep 2026
+# Version      : 3.1.26256
 # Comments     : Called at the end of autostart.sh
 # License      : GNU General Public License, version 2.0
-##########################################################################
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# License URL  : https://github.com/RickRomig/i3wm-debian/blob/main/LICENSE
+###############################################################################
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,# but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+###############################################################################
 
 notify_nsfw() {
 	# set flag files in /tmp
@@ -50,10 +51,10 @@ ethernet_status() {
 	[[ $(awk '/ethernet/ {print $3}' <(nmcli dev)) == 'connected' ]] && return 0 || return 1
 }
 
-select_background() {
-	local local_host="${HOSTNAME:-$(hostname)}"
+select_background_directory() {
+	local -r local_host="${HOSTNAME:-$(hostname)}"
+	local -r bg_dirs=("$HOME/Pictures/wallpaper" "$HOME/Pictures/backgrounds" "$HOME/.config/backgrounds")
 	local bg_dir
-	local bg_dirs=("$HOME/Pictures/wallpaper" "$HOME/Pictures/backgrounds" "$HOME/.config/backgrounds")
 	case "$local_host" in
 		hp-850-g3 )
 			if ethernet_status; then
@@ -75,8 +76,7 @@ select_background() {
 					bg_dir=${bg_dirs[2]}
 					notify_new
 				fi
-			fi
-		;;
+			fi ;;
 		hp-8300 | hp-8300-usdt | probook-6570b | hp-800-g1-dm | hp-800-g1-usdt | hp-6005 )
 			if [[ -f "${bg_dirs[1]}" ]]; then
 				bg_dir=${bg_dirs[1]}
@@ -84,8 +84,7 @@ select_background() {
 			else
 				bg_dir=${bg_dirs[2]}
 				notify_new
-			fi
-			;;
+			fi ;;
 		* )
 			if [[ -d "${bg_dirs[0]}" ]]; then
 				bg_dir=${bg_dirs[0]}
@@ -98,11 +97,11 @@ select_background() {
 				notify_new
 			fi
 	esac
-	while true;	do feh --bg-fill --no-fehbg --randomize "$bg_dir"/*;	sleep 300; done &
+	while true;	do feh --bg-fill --no-fehbg --randomize "$bg_dir"/*; sleep 300; done &
 }
 
 main() {
-	select_background
+	select_background_directory
 }
 
 main "$@"
