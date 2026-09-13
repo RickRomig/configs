6bdd26eef8e4906a9fb4193b03904758e125b45d
Richard Romig <rick.romig@gmail.com>
2026-09-13 13:48:41 -0400

    i3/scripts/check_updates.sh: fixed updates variable assignment

--- i3/scripts/check_updates.sh
+++ i3/scripts/check_updates.sh
@@ -7,24 +7,24 @@
 # Author       : Copyright © 2025, Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.com
 # Created      : 22 Aug 2025
-# Last updated : 14 Feb 2026
-# Version      : 1.2.26045
+# Last updated : 13 Sep 2026
+# Version      : 1.2.26256
 # Comments     : Original script by Drew Grif @ https://github.com/drewgrif
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2
 # License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
 ###############################################################################
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,# but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+###############################################################################
 
-updates=$(grep -c 'upgradable' <(apt list --upgradable 2>/dev/null))
+updates=$(grep -c 'upgradable' < <(apt list --upgradable 2>/dev/null))
 
 if [[ "$updates" -eq 0 ]]; then
     echo "Up-to-date"
