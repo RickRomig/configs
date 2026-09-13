68c633a0fd1f987323a66272d2533ca1306006b4
Richard Romig <rick.romig@gmail.com>
2026-09-13 13:50:16 -0400

    i3/scripts/current_wx.sh: get_wx() - changed echo to printf

--- i3/scripts/current_wx.sh
+++ i3/scripts/current_wx.sh
@@ -7,25 +7,26 @@
 # Author       : Copyright © 2025, Richard B. Romig, Mosfanet
 # Email        : rick.romig@gmail.com | rick.romig@mymetronet.com
 # Created      : 22 Aug 2025
-# Last updated : 22 Aug 2025
+# Last updated : 13 Sep 2026
+# Version      : 1.2.26256
 # Comments     :
 # TODO (Rick)  :
 # License      : GNU General Public License, version 2
 # License URL  : https://github.com/RickRomig/scripts/blob/main/LICENSE
 ###############################################################################
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,# but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+###############################################################################
 
 get_wx() {
 	local cur_wx
 	cur_wx=$(curl -s 'wttr.in/Huber+Heights?u&format=%c+%t+%w')
-	echo "$cur_wx"
+	printf "%s" "$cur_wx"
 }
 get_wx
